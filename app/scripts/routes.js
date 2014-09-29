9257a8c61efe12a3761f92207a3095fef31d82e1
witwave <witwave@outlook.com>
2014-09-30 07:30:49 +0800

    router

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -2,16 +2,28 @@ define(['app'], function(app) {
     'use strict';
     app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
         function($locationProvider, $stateProvider, $urlRouterProvider) {
-            $stateProvider.state('main', {
-                url: '/',
-                templateUrl: 'views/main.html'
+            $urlRouterProvider.otherwise('/dashboard');
+            $stateProvider.state('dashboard', {
+                url: '/dashboard',
+                templateUrl: 'views/dashboard.html'
             }).state('login', {
                 url: '/login',
                 templateUrl: 'views/login.html',
                 controller: 'LoginCtrl'
+            }).state('course', {
+                url: '/course',
+                templateUrl: 'views/course/main.html',
+                controller: 'LoginCtrl'
+            }).state('party', {
+                url: '/party',
+                template: '<h1>This Is A State</h1>'
+            }).state('partyDetail', {
+                url: '/party/:partyID/:partyLocation',
+                template: '<h1>This Is {{partyID}} State</h1>'
             });
+            //http://plnkr.co/edit/IzimSVsstarlFviAm7S7?p=preview
+            //http://scotch.io/tutorials/javascript/angular-routing-using-ui-router#reader-mode
             //$locationProvider.html5Mode(true);
-            $urlRouterProvider.otherwise('/');
         }
     ]);
 });
